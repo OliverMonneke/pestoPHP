730bd7a1ea3a2540c50c847c07c38b50b1b1529b
codersquad <codersquad@googlemail.com>
2014-01-03 19:00:25 +0100

    Adding more array operations

--- vendor/Codersquad/Datatype/Collection.php
+++ vendor/Codersquad/Datatype/Collection.php
@@ -73,4 +73,104 @@ class Collection
     {
         return min($array);
     }
+
+    /**
+     * Fill an array with specific values
+     *
+     * @param array $array      The array
+     * @param int   $startIndex Index of first element
+     * @param int   $count      Number of elements to fill
+     * @param mixed $value      Value to fill
+     *
+     * @return array
+     */
+    public static function fillValues($array, $startIndex, $count, $value)
+    {
+        return array_fill($startIndex, $count, $value);
+    }
+
+    /**
+     * Fill an array with specific keys
+     *
+     * @param array $keys  The keys
+     * @param mixed $value The value
+     *
+     * @return array
+     */
+    public static function fillKeys($keys, $value)
+    {
+        return array_fill_keys($keys, $value);
+    }
+
+    /**
+     * Merge 2 arrays
+     *
+     * @param array $array1    First array
+     * @param array $array2    Second array
+     * @param bool  $recursive Recursive
+     *
+     * @return array
+     */
+    public static function merge($array1, $array2, $recursive = FALSE)
+    {
+        if ($recursive)
+        {
+            return array_merge_recursive($array1, $array2);
+        }
+        else
+        {
+            return array_merge($array1, $array2);
+        }
+    }
+
+    /**
+     * Check if key exists in an array
+     *
+     * @param array $array The array
+     * @param mixed $key   The key to look for
+     *
+     * @return bool
+     */
+    public static function existsKey($array, $key)
+    {
+        return array_key_exists($key, $array);
+    }
+
+    /**
+     * Check if a value exists in an array
+     *
+     * @param array $array The array
+     * @param mixed $value The value to look for
+     *
+     * @return bool
+     */
+    public static function existsValue($array, $value)
+    {
+        $returnValue = FALSE;
+        $length = self::length($array);
+
+        for($i = 0;$i < $length; $i++)
+        {
+            if ($value == $array[$i])
+            {
+                $returnValue = TRUE;
+                break;
+            }
+        }
+
+        return $returnValue;
+    }
+
+    /**
+     * Filter an array using a callback function
+     *
+     * @param array $array    The array
+     * @param mixed $callback Callback function
+     *
+     * @return array
+     */
+    public static function filter($array, $callback = NULL)
+    {
+        return array_filter($array, $callback);
+    }
 }
