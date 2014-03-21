0a03bb22bc59ddd94a2e3722a748f7254902c1b6
oliver <codersquad@googlemail.com>
2014-03-21 09:07:18 +0100

    added test suite files

---
+++ tests/TestCase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oliver
+ * Date: 21.03.2014
+ * Time: 09:01
+ */
+
+namespace Codersquad\Tests;
+
+
+class TestCase extends \PHPUnit_Framework_TestCase {
+
+}
+ 
