d7aa4d343058096007b457a93d998978431c1dcd
oliver <codersquad@googlemail.com>
2014-03-23 08:31:27 +0100

    pestoPHP-2 Add parsing .ini file fetaure

---
+++ src/Configuration/Parse/Type/Ini.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oliver
+ * Date: 23.03.2014
+ * Time: 05:36
+ */
+
+namespace Codersquad\Pestophp\Configuration\Parse\Type;
+use Codersquad\Pestophp\Configuration\Parse\AParse;
+use Codersquad\Pestophp\Configuration\Parse\IParse;
+
+/**
+ * Class Ini
+ * @package Codersquad\Pestophp\Configuration\Parse\Type
+ */
+class Ini extends AParse implements IParse
+{
+    /**
+     * @return mixed
+     */
+    public function parse()
+    {
+        // TODO: Implement parse() method.
+    }
+
+} 
